064f0643515f3104aafcae13598cfff0bfd3c074
pritamprasad0 <pritam.prasd@gmail.com>
2016-02-01 22:36:54 +0530

    Created Initial Version Of Junction Folder Application on tab 1

--- FileChanger/MainWindow.xaml.cs
+++ FileChanger/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +13,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+using Microsoft.Win32;
+
 
 namespace FileChanger
 {
@@ -20,12 +24,66 @@ namespace FileChanger
     /// </summary>
     public partial class MainWindow : Window
     {
+        private System.Windows.Forms.FolderBrowserDialog openDialog = 
+            new System.Windows.Forms.FolderBrowserDialog();
+
+        private string folderName =null;
+        
+
         public MainWindow()
         {
             InitializeComponent();
 
+            
             //Add code here
 
         }
+
+        private void SourceButton_Click(object sender, RoutedEventArgs e)
+        {
+           var result = openDialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                var textfromDialog = openDialog.SelectedPath;
+                SourceTextBox.Text = textfromDialog;
+                folderName = Path.GetFileName(SourceTextBox.Text);
+            }
+        }
+
+        private void DestinationButton_Click(object sender, RoutedEventArgs e)
+        {
+            DestinationTextBox.Foreground = Brushes.Black;
+            var result = openDialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                DestinationTextBox.Text = openDialog.SelectedPath;
+            }
+        }
+
+        private void MakeJunctionButton_Click(object sender, RoutedEventArgs e)
+        {
+            var str = new StringBuilder();
+            str.Append("/C mklink /J ");
+            str.Append("\"");
+            str.Append(DestinationTextBox.Text);
+            str.Append("\\");
+            str.Append(folderName);
+            str.Append("\" ");
+            str.Append("\"");
+            str.Append(SourceTextBox.Text);
+            str.Append("\"");
+            //var sourcePath = SourceTextBox.Text;
+            //var destinationPath = DestinationTextBox.Text +"\\"+folderName;
+            //var command = "/C mklink /J " + '"'+destinationPath+'"'+ " " + '"'+sourcePath+'"';
+            Process.Start("cmd", str.ToString());
+        }
+
+        private void Junction_Loaded(object sender, RoutedEventArgs e)
+        {
+            DestinationTextBox.Foreground = Brushes.Gray;
+            DestinationTextBox.Text = @"D:\OneDrive";
+        }
+
+        
     }
 }
